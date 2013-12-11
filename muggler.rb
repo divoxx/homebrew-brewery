72f72b1cba1cc8ec3884a7da89bbe065d5c4a4d1
Rodrigo Kochenburger <divoxx@gmail.com>
2013-12-10 16:22:54 -0800

    Update muggler recipe

--- muggler.rb
+++ muggler.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Muggler < Formula
   homepage 'https://github.com/divoxx/muggler'
-  url 'https://github.com/divoxx/muggler/archive/v0.1.1.tar.gz'
-  sha1 'eb32e14b5c529c231a175ce753713b7e21e61ff4'
+  url 'https://github.com/divoxx/muggler/archive/v0.1.2.tar.gz'
+  sha1 'fcc15fd246c183d900b20ced6d3bcb0ef9f98fdc'
 
   def install
     system "cp -R * \"#{prefix}/\""
