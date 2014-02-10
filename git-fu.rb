745a6d7a205fea8f2601ba5b6626ca3345a6a2b7
Rodrigo Kochenburger <divoxx@gmail.com>
2014-02-10 13:17:22 -0800

    Fix git-fu formula

--- git-fu.rb
+++ git-fu.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GitFu < Formula
   homepage "https://github.com/divoxx/git-fu"
-  url "https://github.com/divoxx/git-fu/archive/0.1.0.tar.gz"
-  sha1 "16248a22f0e6c680a5cbb905ecb31ae1950b595d"
+  url "https://github.com/divoxx/git-fu/archive/v0.1.0.tar.gz"
+  sha1 "887a24811a186d2bb3615f918ce22b268905f38e"
 
   def install
     system "cp -R * \"#{prefix}/\""
