4bf1fe1988240a39ed715890895bd2ff8dba302e
Rodrigo Kochenburger <divoxx@gmail.com>
2014-02-12 13:20:52 -0800

    Upgrade muggler to 0.2.1

--- muggler.rb
+++ muggler.rb
@@ -2,11 +2,20 @@ require 'formula'
 
 class Muggler < Formula
   homepage 'https://github.com/divoxx/muggler'
-  url 'https://github.com/divoxx/muggler/archive/v0.2.0.tar.gz'
-  sha1 '5d165435a10591b64916f3131c87ce4b45c7d34f'
+  url 'https://github.com/divoxx/muggler/archive/v0.2.1.tar.gz'
+  sha1 'dab82fb6ba5fa10faf71b9a56bf768173a6b0930'
 
   def install
     system "cp -R * \"#{prefix}/\""
+  end
+
+  def caveats
+    <<-EOS
+    Run "muggler install" from within each repository you want to activate muggler.
+
+    After an upgrade, make sure you also run "muggler install" so that git hooks gets
+    updated to the latest versions.
+    EOS
   end
 
   test do
