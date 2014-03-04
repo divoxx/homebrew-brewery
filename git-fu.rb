8febc62a32ff62516136ca9611638ebd10b0c26d
Rodrigo Kochenburger <divoxx@gmail.com>
2014-03-04 15:56:23 -0800

    Upgrade git-fu formula

--- git-fu.rb
+++ git-fu.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GitFu < Formula
   homepage "https://github.com/divoxx/git-fu"
-  url "https://github.com/divoxx/git-fu/archive/v0.1.0.tar.gz"
-  sha1 "887a24811a186d2bb3615f918ce22b268905f38e"
+  url "https://github.com/divoxx/git-fu/archive/v0.2.0.tar.gz"
+  sha1 "d566550148d51d13e22cdeefc85d7e97fda5833e"
 
   def install
     system "cp -R * \"#{prefix}/\""
